e625f33ad3f43d3fcdbfc6fdfd4e207c56d94705
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-09-25 09:33:34 -0700

    added NN model

---
+++ src/model/basic_model.py
@@ -0,0 +1,166 @@
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import os
+import tensorflow as tf
+from abc import ABCMeta, abstractmethod
+import json
+
+
+class BasicModel(object):
+    __metaclass__ = ABCMeta
+    __slots__ = ()
+
+    def __init__ (self, config):
+
+        for k,v in vars(config).items():
+            setattr(self, k, v)
+
+        self.optimizer_fn = tf.train.AdamOptimizer
+        self.dtype = tf.float32
+        self.initializer = tf.contrib.layers.xavier_initializer()
+        self.activation_fn = tf.nn.relu
+
+        # self.sw = tf.summary.FileWriter(self.sw_dir, self.graph)
+
+        # @staticmethod
+        # def number_of_parms():
+        #     total_parameters = 0
+        #     for variable in self.graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
+        #         shape = variable.get_shape()
+        #         variable_parameters = 1
+        #         for dim in shape:
+        #             variable_parameters *= dim.value
+        #         total_parameters += variable_parameters
+        #     print('There are {} trainable parameters in model'.format(total_parameters))
+
+
+    def init(self):
+        # This function is usually common to all your models
+        # but making separate than the __init__ function allows it to be overidden cleanly
+        # this is an example of such a function
+        checkpoint = tf.train.get_checkpoint_state(self.ckpt_dir)
+        if checkpoint is None:
+            if self.mode == 'train':
+                self.sess.run(self.init_op)
+            else:
+                raise ValueError('Model not found to restore.')
+        else:
+            print('Loading model from folder: %s' % self.ckpt_dir)
+            self.saver.restore(self.sess, checkpoint.model_checkpoint_path)
+
+    def __call__ (self, embedding_dims):
+        for k,v in embedding_dims.items():
+            setattr(self, k, v)
+
+        self.graph = self.build_graph()
+        with self.graph.as_default():
+            self.saver = tf.train.Saver(max_to_keep=10)
+            self.init_op = tf.global_variables_initializer()
+
+        if not os.path.exists(os.path.join(self.result_dir,self.model_name)):
+            os.makedirs(os.path.join(self.result_dir,self.model_name))
+        self.ckpt_dir = os.path.join(self.result_dir, self.model_name, 'chekpoints')
+
+        self.sess_config = tf.ConfigProto(allow_soft_placement=True)
+        self.sess_config.gpu_options.allow_growth=True
+        self.sess = tf.Session(config=self.sess_config, graph=self.graph)
+
+        self.init()
+
+        return self
+
+    @abstractmethod
+    def build_graph(self, graph):
+        raise NotImplementedError
+
+
+    def embedding(self, input, shape, dropout, is_train, names):
+        embed_mat = tf.get_variable(shape=shape, name=names[0])
+        embeddings = tf.nn.embedding_lookup(embed_mat, input)
+        return tf.layers.dropout(
+                            embeddings,
+                            dropout,
+                            training=is_train,
+                            name=names[1])
+
+    def _single_cell(self, nhidden, dropout, is_training):
+        if self.layer_norm:
+            _cell_fn = tf.contrib.rnn.LayerNormBasicLSTMCell
+        else:
+            _cell_fn = tf.contrib.rnn.BasicLSTMCell
+        _cell = _cell_fn(nhidden)
+        keep_prob = tf.cond(is_training, lambda:1. - dropout, lambda:tf.constant(1.0))
+        _cell = tf.contrib.rnn.DropoutWrapper(_cell,
+                            output_keep_prob=keep_prob)
+        return _cell
+
+    def _multi_cell(self, nhidden, dropout, is_training, n_layers, is_stack=False):
+        _cells = [self._single_cell(nhidden, dropout, is_training)]
+        for _ in range(1,n_layers):
+            if is_stack:
+                nhidden *= 2
+            _cells.append(tf.contrib.rnn.ResidualWrapper(
+                self._single_cell(nhidden, dropout, is_training)))
+        return _cells
+
+    def save(self, filename):
+    # This function is usually common to all your models, Here is an example:
+        global_step = self.sess.run(self.global_step)
+        if not os.path.exists(self.ckpt_dir):
+            try:
+                os.makedirs(os.path.abspath(self.ckpt_dir))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+        self.saver.save(self.sess, os.path.join(self.ckpt_dir, filename), global_step)
+
+
+    def freeze_graph(self, output_node_names):
+        if not tf.gfile.Exists(self.ckpt_dir):
+            raise AssertionError(
+                "Export directory doesn't exists. Please specify an export "
+                "directory: %s" % model_dir)
+
+        if not output_node_names:
+            print("You need to supply the name of a node to --output_node_names.")
+            return -1
+
+        # We retrieve our checkpoint fullpath
+        checkpoint = tf.train.get_checkpoint_state(self.ckpt_dir)
+        input_checkpoint = checkpoint.model_checkpoint_path
+
+        # We precise the file fullname of our freezed graph
+        absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+        output_graph = absolute_model_dir + "/frozen_model.pb"
+
+        # We clear devices to allow TensorFlow to control on which device it will load operations
+        clear_devices = True
+
+        # We use a built-in TF helper to export variables to constants
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+            self.sess, # The session is used to retrieve the weights
+            self.graph.as_graph_def(), # The graph_def is used to retrieve the nodes
+            output_node_names.split(",") # The output node names are used to select the usefull nodes
+        )
+        # Finally we serialize and dump the output graph to the filesystem
+        with tf.gfile.GFile(output_graph, "wb") as f:
+            f.write(output_graph_def.SerializeToString())
+        print("%d ops in the final graph." % len(output_graph_def.node))
+
+        return output_graph_def
+
+    def load_graph(self, frozen_graph_filename):
+        # We load the protobuf file from the disk and parse it to retrieve the
+        # unserialized graph_def
+        with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+
+        # Then, we import the graph_def into a new Graph and returns it
+        with tf.Graph().as_default() as graph:
+            # The name var will prefix every op/nodes in your graph
+            # Since we load everything in a new graph, this is not needed
+            tf.import_graph_def(graph_def, name="prefix")
+        return graph
---
+++ src/model/slabel_model.py
@@ -0,0 +1,239 @@
+import tensorflow as tf
+
+from .basic_model import BasicModel
+
+class SlabelModel(BasicModel):
+
+    def __init__ (self, config):
+        BasicModel.__init__(self, config)
+
+
+    def _add_placeholders(self):
+        with tf.variable_scope('placeholders'):
+            """Inputs to be fed to the graph."""
+            #shape = (batch_size, max length of sentence, max lenght of word)
+            self.chars_in = tf.placeholder(tf.int32, [None, None], 'char-in')
+            self.chars_len = tf.placeholder(tf.int32, [None], 'char-seq-len')
+            #shape = (batch_size, max length of sentence)
+            self.words_in = tf.placeholder(tf.int32, [None, None], 'words-input')
+            #shape = (batch_size)
+            self.words_len = tf.placeholder(tf.int32, [None], 'word-seq-len')
+            #shape = (batch_size, max length of sentence)
+            self.tags_in = tf.placeholder(tf.int32, [None, None], 'tag-input')
+            #shape = (batch_size * max length of sentence, max length of label)
+            self.labels_in = tf.placeholder(tf.int32, [None, None], 'label-input')
+            #shape = (batch_size * max length of sentence)
+            self.labels_len = tf.placeholder(tf.int32, [None], 'label-seq-len')
+            #shape = (batch_size * length of sentences)
+            self.targets = tf.placeholder(tf.int32, [None], 'targets')
+            #dropout rate
+            self.is_train = tf.placeholder(tf.bool, shape=(), name='is-train')
+
+    def _add_char_lstm(self):
+        with tf.variable_scope('char-LSTM-Layer', initializer=self.initializer):
+
+            char_embed = self.embedding(
+                                self.chars_in,
+                                [self.nchars, self.char_dim],
+                                self.dropout,
+                                self.is_train,
+                                names=['char-embed','char-embed-dropout'])
+
+            char_cell = self._single_cell(
+                                    self.h_char,
+                                    self.dropout,
+                                    self.is_train)
+
+            _, ch_state = tf.nn.dynamic_rnn(char_cell,
+                                            char_embed,
+                                            sequence_length=self.chars_len,
+                                            dtype=self.dtype,
+                                            scope='char-lstm')
+
+            co_shape = [self.batch_size, -1, self.h_char]
+            self.char_out = tf.reshape(ch_state[1], co_shape)
+
+
+    def _add_word_bidi_lstm(self):
+        """ Bidirectional LSTM """
+        with tf.variable_scope('word-LSTM-Layer'):
+            # Forward and Backward direction cell
+            word_embed = self.embedding(
+                                    self.words_in,
+                                    [self.nwords, self.word_dim],
+                                    self.dropout,
+                                    self.is_train,
+                                    names=['word-embed','word-embed-dropout'])
+
+            tag_embed = self.embedding(
+                                    self.tags_in,
+                                    [self.ntags, self.tag_dim],
+                                    self.dropout,
+                                    self.is_train,
+                                    names=['tag-embed','tag-embed-dropout'])
+
+            word_cell_fw = self._multi_cell(self.h_word,
+                                            tf.constant(self.dropout),
+                                            self.is_train,
+                                            self.n_layers)
+
+            word_cell_bw = self._multi_cell(self.h_word,
+                                            tf.constant(self.dropout),
+                                            self.is_train,
+                                            self.n_layers)
+
+            w_bidi_in = tf.concat([word_embed, tag_embed, self.char_out], -1,
+                                        name='word-bidi-in')
+
+            # Get lstm cell output
+            w_bidi_out , _ = tf.nn.bidirectional_dynamic_rnn(
+                                tf.contrib.rnn.MultiRNNCell(word_cell_fw),
+                                tf.contrib.rnn.MultiRNNCell(word_cell_bw),
+                                w_bidi_in,
+                                sequence_length=self.words_len,
+                                dtype=self.dtype)
+            w_bidi_out_c = tf.concat(w_bidi_out , -1, name='word-bidi-out')
+
+            encode_state = tf.concat([w_bidi_in, w_bidi_out_c], -1)
+
+            self.encode_state = tf.layers.dense(encode_state, self.h_label)
+
+    def _add_label_lstm_layer(self):
+        """Generate sequences of tags"""
+        with tf.variable_scope('tag-LSTM-Layer'):
+            label_embed = self.embedding(
+                                self.labels_in,
+                                [self.nlabels, self.label_dim],
+                                self.dropout,
+                                self.is_train,
+                                names=['label-embed','label-embed-dropout'])
+
+            dec_init_state = tf.reshape(self.encode_state, [-1, self.h_label])
+
+            self.label_init = tf.contrib.rnn.LSTMStateTuple(
+                                        dec_init_state,
+                                        tf.zeros_like(dec_init_state))
+
+            label_cell = self._single_cell(self.h_label, self.dropout, self.is_train)
+
+            self.decode_out, self.decode_state = tf.nn.dynamic_rnn(
+                                                label_cell,
+                                                label_embed,
+                                                initial_state=self.label_init,
+                                                sequence_length=self.labels_len,
+                                                dtype=self.dtype)
+
+    def _add_attention(self):
+        with tf.variable_scope('Attention'):
+            es_shape = tf.shape(self.encode_state)[0]
+            k = tf.layers.dense(self.decode_out, self.attention_dim)
+            atten_k = tf.reshape(k, [es_shape, -1, self.attention_dim])
+
+            atten_q = tf.layers.dense(
+                                self.encode_state,
+                                self.attention_dim,
+                                activation=self.activation_fn)
+
+            alpha = tf.nn.softmax(tf.einsum('aij,akj->aik', atten_k, atten_q))
+            context = tf.einsum('aij,ajk->aik', alpha, self.encode_state)
+            context = tf.reshape(context, tf.shape(self.decode_out))
+
+            self.attention = tf.concat([self.decode_out, context], -1)
+
+    def _add_projection(self):
+        with tf.variable_scope('probabilities'):
+
+            logits = tf.layers.dense(
+                            self.attention,
+                            self.projection_dim,
+                            activation=self.activation_fn)
+
+            mask_t = tf.sequence_mask(self.labels_len, dtype=tf.int32)
+            v = tf.dynamic_partition(logits, mask_t, 2)[1]
+
+            self.logits = tf.layers.dense(v, self.nlabels)
+            # compute softmax
+            self.probs = tf.nn.softmax(self.logits, name='probs')
+
+    def _add_loss(self):
+
+        with tf.variable_scope("loss"):
+            targets_1hot = tf.one_hot(self.targets, self.nlabels)
+
+            self.loss = tf.losses.softmax_cross_entropy(
+                                logits=self.logits,
+                                onehot_labels=targets_1hot,
+                                reduction=tf.losses.Reduction.MEAN)
+
+    def _add_train_op(self):
+        self.global_step = tf.Variable(
+                                initial_value=0,
+                                trainable=False,
+                                dtype=tf.int32,
+                                name='g_step')
+
+        self.epoch = tf.Variable(
+                            initial_value=0,
+                            trainable=False,
+                            dtype=tf.int32,
+                            name='epoch')
+
+        self.optimizer = self.optimizer_fn().minimize(
+                                                self.loss,
+                                                global_step=self.global_step)
+
+    def build_graph(self):
+        with tf.Graph().as_default() as g:
+            with tf.device('/gpu:{}'.format(self.gpu_id)):
+                with tf.variable_scope('slabel', initializer=self.initializer, dtype=self.dtype):
+                    self._add_placeholders()
+                    self._add_char_lstm()
+                    self._add_word_bidi_lstm()
+                    self._add_label_lstm_layer()
+                    self._add_attention()
+                    self._add_projection()
+                    self._add_loss()
+                    self._add_train_op()
+            return g
+
+
+        """"TRAIN Part """
+    def step(self, batch, output_feed, is_train=False):
+        """ Training step, returns the loss"""
+        input_feed = {
+            self.words_in: batch.words.input,
+            self.words_len: batch.words.length,
+            self.chars_in : batch.chars.input,
+            self.chars_len : batch.chars.length,
+            self.tags_in : batch.tags.input,
+            self.labels_in: batch.labels.input,
+            self.labels_len: batch.labels.length,
+            self.targets: batch.targets,
+            self.is_train : is_train}
+        import pdb; pdb.set_trace()
+        return self.sess.run(output_feed, input_feed)
+
+    """"Decode Part """
+    def encode_top_state(self, enc_bv):
+        """Return the top states from encoder for decoder."""
+        input_feed = {self.words_in: enc_bv.words.input,
+                        self.words_len: enc_bv.words.length,
+                        self.chars_in : enc_bv.chars.input,
+                        self.chars_len : enc_bv.chars.length,
+                        self.tags_in: enc_bv.tags.input,
+                        self.is_train : False}
+        return self.sess.run(self.encode_state, input_feed)
+
+    def decode_topk(self, latest_tokens, dec_init_states, enc_state, k):
+        """Return the topK results and new decoder states."""
+        input_feed = {
+            self.label_init : dec_init_states,
+            self.labels_in: np.array(latest_tokens),
+            self.encode_state : enc_state,
+            self.labels_len: np.ones(len(latest_tokens), np.int32),
+            self.is_train : False}
+        output_feed = [self.decode_state, self.probs]
+        states, probs = self.sess.run(output_feed, input_feed)
+        topk_ids = np.array([np.argsort(np.squeeze(p))[-k:] for p in probs])
+        topk_probs = np.array([p[k_id] for p,k_id in zip(probs,topk_ids)])
+        return topk_ids, topk_probs, states
