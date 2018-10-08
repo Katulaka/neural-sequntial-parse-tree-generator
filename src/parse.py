a3d2f1473ff5664991680fa9877180a8f9d25ff0
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-10-08 11:33:45 -0700

    debug

--- src/parse.py
+++ src/parse.py
@@ -196,6 +196,7 @@ class Parser(object):
                         row.append((partial_tree, hyp[1]))
                 grid.append(row)
 
+            import pdb; pdb.set_trace()
 
             nodes = astar_search(grid, self.keep_valence_value, astar_parms)
             if nodes is not None:
