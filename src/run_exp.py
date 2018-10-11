4754ad0576a643baf3e1648bd9ed620e06635067
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-10-11 14:33:05 -0700

    new expertiments

--- src/run_exp.py
+++ src/run_exp.py
@@ -5,8 +5,8 @@ import itertools
 proc = []
 n_gpus = 8
 s_gpu = 1
-dropouts = [0.1, 0.2, 0.3, 0.4]
-iter = itertools.product([0.2], dropouts)
+dropouts = [0.1, 0.2, 0.3, 0.4, 0.5]
+iter = itertools.product([0.3], dropouts)
 for i, drops in enumerate(iter):
     path = ("models/tf-with-new-dims-n-tags-dropouts[{},{}]").format(*drops)
     command = ("python src/main.py train --layer-norm "
